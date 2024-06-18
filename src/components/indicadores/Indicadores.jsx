9a85a2ae7adafa2c79e805576e4bbf1427379191
Jeysson Henriquez <jeissonhenrriquez@gmail.com>
2024-06-17 19:12:59 -0500

    diseño

--- src/components/indicadores/Indicadores.jsx
+++ src/components/indicadores/Indicadores.jsx
@@ -1,31 +1,37 @@
 export const Indicadores = () => {
   return (
     <section className='indicadores'>
-      <h2>Indicadores como lider</h2>
+      <h2 style={{ color: 'black' }}>Indicadores como lider</h2>
       <div className='cards_indicadores'>
-        <div className='card_indicador'>
-          <span>3.0</span>
-          <p>CUMPLIMIENTO CRONOGRAMA DE DESARROLLO</p>
+        <div className='row'>
+          <div className='card_indicador'>
+            <span>3.0</span>
+            <p>CUMPLIMIENTO CRONOGRAMA DE DESARROLLO</p>
+          </div>
         </div>
-        <div className='card_indicador'>
-          <span>2.8</span>
-          <p>NPS</p>
+        <div className='row'>
+          <div className='card_indicador'>
+            <span>2.8</span>
+            <p>NPS</p>
+          </div>
+          <div className='card_indicador'>
+            <span>2.8</span>
+            <p>FEEDBACK</p>
+          </div>
         </div>
-        <div className='card_indicador'>
-          <span>2.8</span>
-          <p>FEEDBACK</p>
-        </div>
-        <div className='card_indicador'>
-          <span>2.8</span>
-          <p>SOPORTES PROMEDIO DE SOFTWARES PROPIOS</p>
-        </div>
-        <div className='card_indicador'>
-          <span>3.0</span>
-          <p>CLIMA</p>
-        </div>
-        <div className='card_indicador'>
-          <span>3.0</span>
-          <p>ROTACIÓN</p>
+        <div className='row'>
+          <div className='card_indicador'>
+            <span>2.8</span>
+            <p>SOPORTES PROMEDIO DE SOFTWARES PROPIOS</p>
+          </div>
+          <div className='card_indicador'>
+            <span>3.0</span>
+            <p>CLIMA</p>
+          </div>
+          <div className='card_indicador'>
+            <span>3.0</span>
+            <p>ROTACIÓN</p>
+          </div>
         </div>
       </div>
     </section>

94872f87b7ac99998ab33a63bd29cf5deb0cfd7e
Jeysson Henriquez <jeissonhenrriquez@gmail.com>
2024-06-19 09:44:23 -0500

    plan carrera section

---
+++ src/components/PlanCarrera/PlanCarrera.jsx
@@ -0,0 +1,97 @@
+export const PlanCarrera = () => {
+  return (
+    <section className='plan-carrera'>
+      <h2>PLAN CARRERA</h2>
+      <h2>
+        MIS METAS EN <br /> BUHO A UN AÑO
+      </h2>
+      <div className='container-desarrollar'>
+        <p>
+          Desarrollar y aplicar habilidades expertas en inteligencia artificial
+          y <br /> modelos de lenguaje de largo alcance (LLM) para implementar
+          módulos <br /> de LLM IA en parte de nuestras operaciones que
+          requieren <br /> procesamiento de lenguaje natural. Este desarrollo
+          permitirá la creación <br /> de nuevos servicios, tanto internos como
+          externos.
+        </p>
+      </div>
+      <div className='container-lograr'>
+        <span>LO QUE QUIERO LOGRAR ESTE AÑO</span>
+      </div>
+      <div className='flex-logros'>
+        <div className='container-logros'>
+          <div>
+            <p>
+              Establecer y seguir un plan semanal de gestión del tiempo que
+              equilibre efectivamente mi vida personal, profesional y mis
+              estudios, dedicando horas específicas a cada área y actividades de
+              relajación, para mantener un alto nivel de salud mental. Este plan
+              incluirá evaluaciones mensuales de mi bienestar emocional y
+              ajustes necesarios, con el objetivo de alcanzar y mantener un
+              equilibrio sostenible durante todo el año.
+            </p>
+          </div>
+          <div>
+            <p>
+              Inscribirme en un programa de posgrado en ciberseguridad o
+              inteligencia artificial en una universidad antes de agosto de este
+              año para desarrollar habilidades avanzadas y conocimientos
+              especializados que mejoren mis capacidades profesionales en el
+              campo tecnológico.
+            </p>
+          </div>
+          <div>
+            <p>
+              Crear y seguir un plan financiero personalizado para ahorrar un
+              mínimo del 20% de mis ingresos mensuales, empezando este mes, con
+              el fin de acumular suficientes fondos para viajar por algunos
+              destinos de Colombia en un plazo de un año. Revisaré y ajustaré mi
+              presupuesto trimestralmente para asegurar que estoy en camino de
+              alcanzar mi meta de viajes
+            </p>
+          </div>
+        </div>
+        <div className='container-qs'>
+          <div>
+            <p>
+              Liderar el desarrollo e implementación de una infraestructura
+              tecnológica escalable y sostenible capaz de soportar la "
+              {'Nueva metodologia'}" a nivel de big data, comenzando con un
+              análisis exhaustivo de las necesidades actuales y proyectadas en
+              los próximos tres meses. Con el objetivo de tener el sistema
+              completamente operativo y optimizado.
+            </p>
+          </div>
+          <div>
+            <p>
+              realizar un análisis exhaustivo de las capacidades tecnológicas
+              actuales de buho, para mejorar la competitividad. Desarrollar
+              indicadores de progreso para cada área y completar el análisis y
+              el informe con un plan de implementación para el final del tercer
+              trimestre del año.
+            </p>
+          </div>
+          <div>
+            <p>
+              Adquirir conocimientos en infraestructura en AWS a través de la
+              participación en formaciones y/o diplomados, con el propósito de
+              poder ofrecer internamente cualquier servicio en la nube sin
+              depender de terceros para la implementación de desarrollos. El
+              objetivo final es aumentar la autonomía y capacidad técnica del
+              equipo en la gestión e implementación de servicios en la nube.
+            </p>
+          </div>
+          <div>
+            <p>
+              Desplegar un modelo de lenguaje de largo alcance (LLM),
+              reentrenado con datos propios, para uso interno. El objetivo es
+              asegurar la privacidad y seguridad de nuestra información
+              sensible, al mismo tiempo que se potencia el domain knowledge y se
+              hace más accesible y global para todos en buho.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};

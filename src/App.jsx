80e20d917083f9fb67fac2ecca1982da7925f7e4
Marcos Alape <marcos.alape@gnilat.com>
2024-06-18 21:38:18 -0500

    ordenar importaciones en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css';
-import About from './components/about/About';
-import ConfirmSection from './components/ConfirmSection';
-import { Funciones } from './components/funciones/Funciones';
-import Home from './components/Home';
+import { Testimonios } from './components/testimonios/Testimonios';
 import { Indicadores } from './components/indicadores/Indicadores';
-import { KPI } from "./components/KPI'S/KPI";
+import { Funciones } from './components/funciones/Funciones';
 import { Liderazgo } from './components/liderazgo/Liderazgo';
+import { Propuestas } from './components/propuestas/Propuestas';
+import { KPI } from "./components/KPI'S/KPI";
+
+import Tecnologias from './components/tecnologias/Tecnologias';
+import ConfirmSection from './components/ConfirmSection';
 import Logros from './components/logros/Logros';
 import Navbar from './components/Navbar';
 import Piloto from './components/piloto/Piloto';
-import { Propuestas } from './components/propuestas/Propuestas';
-import Tecnologias from './components/tecnologias/Tecnologias';
-import { Testimonios } from './components/testimonios/Testimonios';
+import About from './components/about/About';
+import Home from './components/Home';
+import './App.css';
 
 function App() {
   return (

727f3d9c12a992bb160d7f53e7ebb6bff13b1982
Jeysson Henriquez <jeissonhenrriquez@gmail.com>
2024-07-04 11:10:21 -0500

    pngs en mision experto

--- src/components/misionExperto/MisioExperto.jsx
+++ src/components/misionExperto/MisioExperto.jsx
@@ -1,9 +1,9 @@
 import imgGroup from '../../assets/img/group.svg';
-import imgSena from '../../assets/img/Sena.jpeg';
-import imgJaveriana from '../../assets/img/Javeriana.jpeg';
-import imgAgustiniana from '../../assets/img/Agustiniana.jpeg';
-import imgMinTic from '../../assets/img/MinTic.jpeg';
-import imgCCPL from '../../assets/img/CCPL.jpeg';
+import imgSena from '../../assets/img/Sena.png';
+import imgJaveriana from '../../assets/img/Javeriana.png';
+import imgAgustiniana from '../../assets/img/Agustiniana.png';
+import imgMinTic from '../../assets/img/MinTic.png';
+import imgCCPL from '../../assets/img/CCPL.png';
 
 function MisionExperto() {
   return (
